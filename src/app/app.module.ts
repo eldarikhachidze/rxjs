de1476e9ad4f7fe0660866fdd0d4eb04a75ea174
eldari <xachidze.eldari@gmail.com>
2022-12-14 22:41:18 +0400

    some:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { subjectComponent } from './subject/subject.component';
+import { behaviorSubjectComponent } from './behavior-subject/behavior-subject.component';
+import { ReplaySubjectComponent } from './replay-subject/replay-subject.component';
+import { AsyncSubjectComponent } from './async-subject/async-subject.component';
+import { OperatorsComponent } from './operators/operators.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    subjectComponent,
+    behaviorSubjectComponent,
+    ReplaySubjectComponent,
+    AsyncSubjectComponent,
+    OperatorsComponent
   ],
   imports: [
     BrowserModule,
